7dcbd30d061b916a049629ae3d2e0e8ea4e4c80e
Jakob Weimar <jakob.weimar@andrena.de>
2024-05-23 14:58:14 +0200

    Add caching

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{sync::Arc, time::Duration};
+use moka::future::Cache;
 
 use axum::{
     extract::{Path, State},
-    response::{Html, IntoResponse, Response},
+    response::{IntoResponse, Response},
     routing::get,
     Json, Router,
 };
 use axum_macros::debug_handler;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -14,8 +16,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(2))
         .build()?;
+    let cached_client = PokemonCachedClient::new(&client);
     // build our application with a route
-    let state = RequestState { client };
+    let state = RequestState { cached_client };
     let app = Router::new().route("/:pokemon_name", get(handler)).with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -28,8 +31,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[derive(Clone)]
-struct RequestState {
+struct PokemonCachedClient {
     client: reqwest::Client,
+    cache: Cache<String, Pokemon>,
+}
+
+impl PokemonCachedClient {
+    fn new(client: &Client) -> PokemonCachedClient {
+        Self {
+            client: client.clone(),
+            cache: Cache::new(100),
+        }
+    }
+
+    pub async fn get_pokemon_by_name(&self, pokemon_name: String) -> Result<Pokemon, Arc<reqwest::Error>> {
+        self.cache.try_get_with(pokemon_name.clone(), self.fetch_pokemon_by_name(&pokemon_name)).await
+    }
+
+    async fn fetch_pokemon_by_name(&self, pokemon_name: &str) -> Result<Pokemon, reqwest::Error> {
+        let response = self
+            .client
+            .get(format!("https://pokeapi.co/api/v2/pokemon/{pokemon_name}"))
+            .send()
+            .await?;
+        let pokemon = response.json().await?;
+        Ok(pokemon)
+    }
+}
+
+#[derive(Clone)]
+struct RequestState {
+    cached_client: PokemonCachedClient,
 }
 
 #[debug_handler]
@@ -37,22 +69,20 @@ async fn handler(
     State(state): State<RequestState>,
     Path(pokemon_name): Path<String>,
 ) -> Result<Json<Pokemon>, AppError> {
-    let response = state
-        .client
-        .get(format!("https://pokeapi.co/api/v2/pokemon/{pokemon_name}"))
-        .send()
+    let pokemon = state
+        .cached_client
+        .get_pokemon_by_name(pokemon_name)
         .await?;
-    let pokemon = response.json().await?;
     Ok(Json(pokemon))
 }
 
 #[derive(Debug)]
 enum AppError {
-    ClientError(reqwest::Error),
+    ClientError(Arc<reqwest::Error>),
 }
 
-impl From<reqwest::Error> for AppError {
-    fn from(value: reqwest::Error) -> Self {
+impl From<Arc<reqwest::Error>> for AppError {
+    fn from(value: Arc<reqwest::Error>) -> Self {
         AppError::ClientError(value)
     }
 }
